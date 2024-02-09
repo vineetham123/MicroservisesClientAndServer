de45d9f601e7e5315c851f66b33bfef20cc518b3
vineetham123 <145333794+vineetham123@users.noreply.github.com>
2024-02-09 15:33:29 +0530

    Added the client and server example

---
+++ spring-cloud-config-client-example/src/main/java/com/example/springcloudconfigclientexample/SpringCloudConfigClientExampleApplication.java
@@ -0,0 +1,32 @@
+package com.example.springcloudconfigclientexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/show")
+public class SpringCloudConfigClientExampleApplication {
+
+          
+         
+
+    @Autowired
+    private Environment env;
+    
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClientExampleApplication.class, args);
+	}
+	@GetMapping
+	  public  String showMessage(){
+	  
+	  return env.getProperty("message");
+          }
+
+}
---
+++ spring-cloud-config-client-example/src/test/java/com/example/springcloudconfigclientexample/SpringCloudConfigClientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudconfigclientexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigClientExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
